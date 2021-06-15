de1f544c08897f34b8740ac238775a1be1ba8abb
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2021-06-15 21:49:52 +0700

    add provinsi seeder

--- database/seeders/ProvinsiSeeder.php
+++ database/seeders/ProvinsiSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProvinsiSeeder extends Seeder
 {
@@ -13,6 +14,21 @@ class ProvinsiSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $file = fopen(base_path() . '/sql/provinsi.csv', 'r');
+        $payload = [];
+
+        while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {
+            $payload[] = [
+                'nama_provinsi' => $data[0],
+            ];
+        }
+        fclose($file);
+
+        $acc = 0;
+        foreach (array_chunk($payload, 1000) as $p) {
+            DB::table('provinsis')->insert($p);
+            $acc += count($p);
+            $this->command->line('Processed ' . $acc . ' rows');
+        }
     }
 }
